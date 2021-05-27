24e4d7c0bd68aba3bfd081a24fb4752ded79cb0e
iemong <kiyoshidainagon@gmail.com>
2021-05-28 00:48:40 +0900

    途中までcommit

--- index.ts
+++ index.ts
@@ -14,11 +14,70 @@ try {
 
   const notes = doc.querySelectorAll(".o-textNote");
 
-  const linkList = Array.from(notes).map(($note) => {
+  const myMap = new Map<string, string>();
+
+  Array.from(notes).forEach(($note) => {
     const $anchor = ($note as Element).querySelector(".o-textNote__link");
-    return $anchor?.getAttribute("href");
+    const $title = ($note as Element).querySelector(".o-textNote__title > a");
+    const link = $anchor?.getAttribute("href") || "";
+    myMap.set(
+      $title?.textContent.trim() || "",
+      /^https:\/\//.test(link) ? link : `https://note.com${link}`,
+    );
   });
-  console.log(linkList);
+
+  console.log(myMap);
+
+  const databaseResponse = await fetch(
+    `https://api.notion.com/v1/databases/${config().DATABASE_ID}/query`,
+    {
+      method: "post",
+      headers: new Headers({
+        "Authorization": `Bearer ${config().API_KEY}`,
+        "Content-Type": "application/json",
+        "Notion-Version": "2021-05-13",
+      }),
+      body: JSON.stringify({
+        "page_size": 100,
+      }),
+    },
+  );
+  const database = await databaseResponse.json();
+  const urlList = database.results.map((d: any) => d.properties.URL.url);
+  console.log(urlList);
+
+  // TODO 1秒間に3回までしかリクエストできないらしいから、時間をずらす
+  const fetchers = Array.from(myMap.values()).map((k) => {
+    if (urlList.includes(myMap.get(k))) return;
+    return fetch("https://api.notion.com/v1/pages", {
+      method: "post",
+      headers: new Headers({
+        "Authorization": `Bearer ${config().API_KEY}`,
+        "Content-Type": "application/json",
+        "Notion-Version": "2021-05-13",
+      }),
+      body: JSON.stringify({
+        "parent": { "database_id": config().DATABASE_ID },
+        "properties": {
+          "Name": {
+            "title": [
+              {
+                "text": {
+                  "content": k,
+                },
+              },
+            ],
+          },
+          "URL": {
+            "url": myMap.get(k),
+          },
+        },
+      }),
+    });
+  }).filter((fetcher) => !!fetcher);
+  await Promise.all(fetchers).then((a) => {
+    console.log(a);
+  });
 } catch (e) {
   console.error(e);
 }
